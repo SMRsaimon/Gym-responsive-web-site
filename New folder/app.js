32dedd1b56a79aabfab6b354552570b2d5655bab
Sultan Mahmud <smrsaimon960@gmail.com>
2021-02-19 20:46:02 +0600

    jj

--- New folder/app.js
+++ New folder/app.js
@@ -1,7 +1,16 @@
-const triggerButton = (id) => {
-    const questionOne = document.getElementById(id);
+// const triggerButton = (id) => {
+//     const questionOne = document.getElementById(id);
 
-    setTimeout(() => {
-        questionOne.classList.toggle('d-none');
-    }, 500);
-};
+//     setTimeout(() => {
+//         questionOne.classList.toggle('d-none');
+//     }, 500);
+// };
+
+let number = 10;
+
+let fact = 1;
+
+for (let i = 0; i < number.length; i++) {
+    let element = number[i];
+    console.log(element);
+}
